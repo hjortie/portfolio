0beff31fd5bc7a22bc8c6fae8743cb91cc0fdf09
hjortie <elinhjortskull@live.se>
2025-05-19 14:29:50 +0200

    added basename to the react router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,15 +5,18 @@ import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
 import { Skills } from "./components/Skills/Skills";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/skills", element: <Skills /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/projects", element: <Projects /> },
+        { path: "/skills", element: <Skills /> },
+        { path: "/contact", element: <Contact /> },
+      ],
+    },
+  ],
+  { basename: "/portfolio" }
+);
