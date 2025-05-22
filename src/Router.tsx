ee601c469d574d07da1df9e674621c18502c3f1a
hjortie <elinhjortskull@live.se>
2025-05-22 11:03:10 +0200

    updated Router imports

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router";
-import { Layout } from "./components/Layout/Layout";
-import { Projects } from "./components/Projects/Projects";
-import { Contact } from "./components/Contact/Contact";
-import { Home } from "./components/Home/Home";
-import { Skills } from "./components/Skills/Skills";
+import { Layout } from "./pages/Layout/Layout";
+import { Projects } from "./pages/Projects/Projects";
+import { Contact } from "./pages/Contact/Contact";
+import { Home } from "./pages/Home/Home";
+import { Skills } from "./pages/Skills/Skills";
 
 export const router = createBrowserRouter(
   [
